c4675cf18966d407ca70932381fe4166aa15a1bd
PepitoSama <etienne.saimond@gmail.com>
2019-04-13 20:20:24 +0200

    Mise a jour de la spécification fonctionnelle des fonctions du serveur

--- Serveur/serveur.c
+++ Serveur/serveur.c
@@ -9,7 +9,7 @@
 #include <signal.h>
 
 // Taille du buffer
-#define TAILLE_BUFFER 1024
+#define TAILLE_BUFFER 4096
 
 // Port(s) utilisé par le socket
 int dS;
@@ -42,8 +42,8 @@ int main () {
     //déclaration de variables annexes
     int booleen1 = 1;
     int nbConnexion = 2;
-    char buffer1[4096];
-    char buffer2[4096];
+    char buffer1[TAILLE_BUFFER];
+    char buffer2[TAILLE_BUFFER];
 
     bind_server(ad);
     listen_server(2);
@@ -115,6 +115,8 @@ int attendreConnexion(int numClient) {
 /*
 * func gestionDecoClient : int, char[] ->
 * Gère la deconnexion d'un client
+* Attend la connexion d'un nouveau client
+* Envoie au nouveau client le dernier message envoyé
 */
 void gestionDecoClient(int numClient, char *buffer) {
     printf("Client %d deconnecté, en attente d'un nouveau client\n", numClient);
@@ -151,16 +153,13 @@ void envoyerMessage(int numClient,char *buffer) {
         exit(1);
     } else {
         printf("Message envoyé : %s\n", buffer);
-        if (strcmp(buffer, "fin") == 0) {
-            closeAllPort();
-        }
     }
 }
 
 
 
 /*
-* func closeAllPort : int ->
+* func closeAllPort : ->
 * Fonction appelée si l'utilisateur appuie sur CTRL+C
 * Envoie aux deux client "exit" et ferme tout les ports
 */
@@ -177,11 +176,10 @@ void closeAllPort() {
 
 /*
 * func recevoirMessage : int, char[], char[]->
-* Envoie a partir d'un socket, un tableau de charactère un message
-* Renvoie 0 si aucune Erreur
-* Renvoie -1 si erreur
-* Renvoie -2 si chaine trop longue
-* Arrete le programme via la fonction closeAllPort si le contenue de la chaine est "fin "
+* Recoit le message d'un client à partir du numéro du client
+* , un tableau de charactère pour recevoir le message
+* et un tableau de charactère contenant le dernier message envoyé en cas de deconnexion du client.
+* Attend la connexion d'un nouveau client si le contenue de la chaine est "fin "
 */
 
 void recevoirMessage(int numClient,char *bufferReception, char *bufferPrecedent) {
@@ -197,29 +195,41 @@ void recevoirMessage(int numClient,char *bufferReception, char *bufferPrecedent)
         exit(1);
     }
 
-    // Vérification du retoure de la fonction
+    // Vérification du retour de la fonction
     if (res < 0) {
         perror("recevoirMessage");
         exit(1);
     } else {
+        // Remplace le dernier charactère par '\0' pour éviter toute erreur
+        // lié a une chaine trop longue ou mal formattée
         bufferReception[res] = '\0';
         if (strcmp( bufferReception, "fin") == 0) {
+            // Si un client se deconnecte, lance la procedure de gestion de deconnexion d'un client
             gestionDecoClient(numClient, bufferPrecedent);
             recevoirMessage(numClient, bufferReception, bufferPrecedent);
         } else if (strcmp( bufferReception, "exit") == 0) {
-            strcpy(bufferReception, "Votre interlocutteur veut votre mort :/");
+            // Envoie de exit avec un espace, sinon le client va se deconnecter a la reception du message
+            strcpy(bufferReception, "exit ");
         }
     }
 }
 
+/*
+* func init_socket : ->
+* Initialise le socket
+*/
 void init_socket() {
-    //Definition du protole
+    //Definition du protole (TCP ici)
     if ((dS = socket(PF_INET,SOCK_STREAM,0)) == -1) {
         perror("socket");
         exit(1);
     }
 }
 
+/*
+* func init_server : ->
+* Initialise les paramètres du serveur
+*/
 struct sockaddr_in init_server(int port) {
     // définition des paramètres du serveur
     struct sockaddr_in ad;
@@ -229,6 +239,10 @@ struct sockaddr_in init_server(int port) {
     return ad;
 }
 
+/*
+* func bind_server : ->
+* Assigne l'adresse ip et le port au serveur
+*/
 void bind_server(struct sockaddr_in ad) {
     if ((bind(dS, (struct sockaddr*)&ad, sizeof(ad))) == -1) {
         perror("bind");
@@ -236,6 +250,10 @@ void bind_server(struct sockaddr_in ad) {
     }
 }
 
+/*
+* func listen_server : ->
+* Definit le nombre de connexion maximum accepté par le socket
+*/
 void listen_server(int nbConnexion) {
     if ((listen(dS,nbConnexion)) == -1) {
         perror("listen");
